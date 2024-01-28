0573d0f3848c84cfa2244d1ae6338efcb9a11d74
Bryan Paul Razón Machain <73920749+PaulRazon@users.noreply.github.com>
2024-01-27 19:31:39 -0700

    Deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images:{
     formats:['image/avif','image/webp'],
     domains:['res.cloudinary.com']
